fd714dd8472ec160edc10b9264a66a3b967cd5d4
Patrick <patrick@mp.ls>
2026-08-04 17:57:11 -0500

    Clean prefix reset: single iconv block, local UnsignedWide, no Universal MacTypes, SIZE 64/16 MB

--- browser/netsurf/frontends/macos9/macsurf_prefix.h
+++ browser/netsurf/frontends/macos9/macsurf_prefix.h
@@ -170,6 +170,24 @@ void macsurf_assert_failed_(const char *expr, const char *file, int line);
 #include <stdio.h>
 
 #ifdef __RETRO68__
+#ifndef __MACTYPES__
+struct UnsignedWide { unsigned long hi; unsigned long lo; };
+typedef struct UnsignedWide UnsignedWide;
+#endif
+extern void Microseconds(UnsignedWide *tickCount);
+typedef unsigned char Boolean;
+typedef void *iconv_t;
+static iconv_t iconv_open_(const char *t, const char *f) { (void)t;(void)f; return (iconv_t)-1; }
+static size_t iconv_(iconv_t cd, char **ib, size_t *il, char **ob, size_t *ol)
+    { (void)cd;(void)ib;(void)il;(void)ob;(void)ol; return (size_t)-1; }
+static int iconv_close_(iconv_t cd) { (void)cd; return -1; }
+#define iconv_open iconv_open_
+#define iconv iconv_
+#define iconv_close iconv_close_
+#endif
+
+
+#ifdef __RETRO68__
 /* Local type definitions (Multiversal MacTypes lacks these) */
 #ifndef __MACTYPES__
 struct UnsignedWide { unsigned long hi; unsigned long lo; };
@@ -433,8 +451,14 @@ extern int   memcmp(const void *, const void *, size_t);
 #ifndef SLEN
 #define SLEN(x) (sizeof((x)) - 1)
 #endif
+#ifndef SLEN
+#define SLEN(x) (sizeof((x)) - 1)
+#endif
 #ifndef NOF_ELEMENTS
 #define NOF_ELEMENTS(x) (sizeof((x)) / sizeof((x)[0]))
+#endif
+#ifndef INT64_MIN
+#define INT64_MIN (-INT64_MAX-1)
 #endif
 #ifndef INT64_MIN
 #define INT64_MIN (-INT64_MAX-1)
